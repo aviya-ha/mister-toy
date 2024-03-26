835cab866ecbf46cfef39d281b1b6db68661c333
aviya-ha <aviyaharel6@gmail.com>
2024-03-26 19:05:22 +0200

    Add loadToys

--- src/services/toy.service.js
+++ src/services/toy.service.js
@@ -1,4 +1,5 @@
 import { aStorageService } from './async-storage.service.js'
+import { utilService } from './util.service.js'
 
 
 const STORAGE_KEY = 'toyDB'
@@ -7,6 +8,8 @@ export const toyService = {
     query,
     getEmptyToy,
 }
+
+_createToys()
 
 function query() {
     return aStorageService.query(STORAGE_KEY).then(toys => toys)
@@ -18,6 +21,36 @@ function getEmptyToy() {
         price: 0,
         labels: [],
         desc: '',
-        inStock: true
+        inStock:(Math.random()>0.5)? true : false
     }
+}
+
+function _createToys() {
+    let toys = utilService.loadFromStorage(STORAGE_KEY)
+    if (!toys || !toys.length) {
+        toys = []
+        toys.push(_createToy('Teddy Bear' , 15))
+        toys.push(_createToy('Rubber Duck' , 50))
+        toys.push(_createToy('Toy Car' , 150))
+        toys.push(_createToy('Toy Train', 200))
+        toys.push(_createToy('Rocking Horse', 185))
+        utilService.saveToStorage(STORAGE_KEY, toys)
+    }
+}
+
+function _createToy(name, price) {
+    const toy = getEmptyToy()
+    toy._id = utilService.makeId()
+    toy.name = name
+    toy.price = +price
+    toy.labels.push(_getRandomLable())
+    toy.desc = utilService.makeLorem(utilService.getRandomIntInclusive(10, 15))
+    toy.creatAt = Date.now()
+    return toy
+}
+function _getRandomLable() {
+    const labels = ['On wheels', 'Box game', 'Art', 'Baby', 'Doll', 'Puzzle',
+        'Outdoor', 'Battery Powered']
+    const num = utilService.getRandomIntInclusive(0, 7)
+    return labels[num]
 }
---
+++ src/store/actions/toy.actions.js
@@ -0,0 +1,16 @@
+import { toyService } from "../../services/toy.service.js"
+import { SET_TOYS } from "../reducers/toy.reducer.js"
+import { store } from "../store.js"
+
+
+
+export function loadToys() {
+    return toyService.query()
+        .then(toys => {
+            store.dispatch({ type: SET_TOYS, toys })
+        })
+        .catch(err => {
+            console.log('toy action -> Cannot load toys', err)
+            throw err
+        })
+}
