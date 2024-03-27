f0a69c5519423e6d99998a98fe07a9af83908ea3
aviya-ha <aviyaharel6@gmail.com>
2024-03-27 19:52:58 +0200

    Fixing unknown problem with BrowserRouter

--- src/root-cmp.jsx
+++ src/root-cmp.jsx
@@ -1,5 +1,5 @@
 import { Provider } from 'react-redux'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter , Route, Routes, HashRouter as Router} from 'react-router-dom'
 import '../src/assets/style/main.css'
 
 import { AppHeader } from './cmps/AppHeader.jsx'
