dae0a4030d51936ceb718a65c452af259f0f7b7d
aviya-ha <aviyaharel6@gmail.com>
2024-04-01 17:04:59 +0300

    update cloudinary

--- src/services/upload.service.js
+++ src/services/upload.service.js
@@ -2,8 +2,8 @@ export const uploadService = {
   uploadImg
 }
 async function uploadImg(ev) {
-  const CLOUD_NAME = "dcwibf9o5"
-  const UPLOAD_PRESET = "vt0iqgff"
+  const CLOUD_NAME = "dp5c3qyef"
+  const UPLOAD_PRESET = "MisterToy"
   const UPLOAD_URL = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`
 
   try {
